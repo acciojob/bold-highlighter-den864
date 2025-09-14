25e9b18383b2b397d6a38cb10c7c35e0aa2683d5
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-14 19:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function highlight() {
 	elements.forEach(ele=>ele.style.color="green")
 
 }
-
 function return_normal() {
 	elements.forEach(ele=>ele.style.color="black")
 }
+
+const target=document.querySelector("p");
+target.addEventListener("mouseover",highlight);
+target.addEventListener("mouseout",return_normal);
